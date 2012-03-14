909ee1843faa0692b2c939f24243954d5c74dac5
Lenna Peterson <arklenna@gmail.com>
2012-03-14 18:45:10 -0400

    Column width detection should work.

--- scripts/datcom2xml.py
+++ scripts/datcom2xml.py
@@ -13,6 +13,8 @@ footer_re = re.compile(r"1\s+")
 
 if __name__ == "__main__":
     headers = []
+    rows = []
+
     if len(sys.argv) == 2:
         filename = sys.argv[1]
         with open(filename) as fh:
@@ -25,13 +27,21 @@ if __name__ == "__main__":
                     print "Found title:", line
                 if title and re.match(header_re, line):
                     header += 1
-                    headers.append(line)
+                    headers.append((header,line))
                     print "Found header:", line
+                if title and header and not re.match(header_re, line):
+                    if line.strip() == "0":
+                        continue
+                    rows.append((header,line))
+                    #print "Found row: %s (%s)" % (line, header) 
                 if title and header and re.match(footer_re, line):
                     print "Found footer:", line
                     break
 
-    for line in headers:
+    breakpoints = []
+    for head_num, line in headers:
+        if len(headers) == 0:
+            break
         line = re.sub(r"^0", " ", line)
         i = re.finditer(r"(\s*)([^\s]+)", line)
         space_sizes = []
@@ -44,17 +54,19 @@ if __name__ == "__main__":
 
         num_cols = len(columns)
 
-        breakpoints = [0]
+        header_breakpoints = [0]
         for n, space, col in zip(xrange(num_cols), space_sizes, columns):
             print n, space, col, 
             print "(%s)" % len(col)
             if n == 0:
-                width = space + len(col) + ceil(space_sizes[n+1]/2)
-            elif n == num_cols:
-                width = floor(space/2) + len(col)
+                width = space + len(col) + int(ceil(space_sizes[n+1]/2))
+            elif n == num_cols-1:
+                width = int(floor(space/2)) + len(col)
             else:
-                width = floor(space/2) + len(col) + ceil(space_sizes[n+1]/2)
-            breakpoints.append(width)
+                width = int(floor(space/2)) + len(col) + int(ceil(space_sizes[n+1]/2))
+            header_breakpoints.append(width)
 
-        print breakpoints
+        breakpoints.append((head_num, header_breakpoints))
+
+    print breakpoints
 # vim:sw=4:ts=4:expandtab
